15cfff2f7c9a0082c80c2c0af28868f390fd0a9a
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-12-18 19:28:23 +0100

    Improve docs

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # memprofiler
 
-A module for monitoring memory usage of IPython cells.
+This is a module for monitoring memory usage of Jupyter notebook cells.
+
+
+## Installation
+
+From pip:
+```
+pip install memprofiler
+```
+
+From source:
+```
+python setup.py install
+``` 
+
+
+## Usage
+
+Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
+
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+## License
+
+Distributed under the BSD-3 License. See `LICENSE` for more information.
+
--- setup.py
+++ setup.py
@@ -6,25 +6,23 @@ Development Status :: 5 - Production/Stable
 Intended Audience :: Science/Research
 Intended Audience :: Developers
 License :: OSI Approved :: BSD License
-Programming Language :: Python
 Programming Language :: Python :: 3
 Topic :: Software Development
-Operating System :: POSIX
-Operating System :: Unix
+Operating System :: OS Independent
 """
 
 
 setup(
-    name='memprofiler',
-    description='A module for monitoring memory usage of IPython cells',
-    long_description=open('README.md').read(),
+    name="memprofiler",
+    description="A module for monitoring memory usage of Jupyter notebook cells",
+    long_description=open("README.md").read(),
     version="2020.1.0",
-    author='Aleix Alcacer',
-    author_email='aleixalcacer@gmail.com',
-    url='https://github.com/aleixalcacer/memprofiler',
-    packages=['memprofiler'],
-    install_requires=['psutil', 'ipython', 'plotly'],
-    python_requires='>=3.4',
-    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-    license=open('LICENSE').read()
+    author="Aleix Alcacer",
+    author_email="aleixalcacer@gmail.com",
+    url="https://github.com/aleixalcacer/memprofiler",
+    packages=["memprofiler"],
+    install_requires=["psutil", "ipython", "plotly"],
+    python_requires=">=3.4",
+    classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
+    license=open("LICENSE").read(),
 )
